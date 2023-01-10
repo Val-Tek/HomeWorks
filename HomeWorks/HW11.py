0efe22795f53d63c4d749bb1a30bb5103654cbd6
Valeriia Tekeli <VashaLera1@gmail.com>
2023-01-10 14:24:27 +0300

    Initial commit

--- HomeWorks/HW11.py
+++ HomeWorks/HW11.py
@@ -1,3 +1,17 @@
-# При помощи функции filter() из котрежа слов отфильтровать только те, которые являются полиндромами
-# (слова которые читаются одинаково в обе стороны), регистр букв не учитывать.
-# inputdata = ['Страна', 'шалаш', 'Летел', 'вертолёт', 'УЧУ', 'мэм', 'язык']
+# При помощи функции filter() из котрежа слов отфильтровать только те, которые являются
+# полиндромами (слова которые читаются одинаково в обе стороны), регистр букв не учитывать.
+
+inputdata = ['Страна', 'шалаш', 'Летел', 'вертолёт', 'УЧУ', 'мэм', 'язык']
+inputdata=tuple(inputdata)
+
+def is_polindrom(word):
+    word = word.upper()
+    return word[::-1] == word
+
+result = list(filter(is_polindrom, inputdata))
+print(result)
+
+#polindrom_inputdata = list(filter(lambda w: w[::-1] == w, inputdata))
+#print(polindrom_inputdata)
+
+
