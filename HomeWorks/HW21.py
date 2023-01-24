261d0ca266115033a7dc4af2d14c9981d55cfbe3
Valeriia Tekeli <VashaLera1@gmail.com>
2023-01-24 16:24:45 +0300

    Initial commit

---
+++ HomeWorks/HW21.py
@@ -0,0 +1,24 @@
+# Создать родительский класс auto, у которого есть атрибуты:
+# brand, age, cоlor, mark и weight.
+# А так же методы: move, birthday и stop.
+# Методы move и stop выводят сообщение на экран «move» и «stop»,а birthday увеличивает атрибут age на 1.
+# Атрибуты brand, age и mark являются обязательными при объявлении объекта.
+
+class Auto():
+    def __init__(self, brand, age, mark, color=None, weight=None):
+        self.brand = brand
+        self.age = age
+        self.mark = mark
+        self.color = color
+        self.weight = weight
+
+    def move(self):
+        print("move")
+
+    def stop(self):
+        print("stop")
+
+    def birthday(self):
+        return self.age + 1
+
+
