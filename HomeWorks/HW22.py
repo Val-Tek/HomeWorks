d9e001bc4a7c168da593dc06b3efe2726f3772e1
Valeriia Tekeli <VashaLera1@gmail.com>
2023-01-24 18:00:59 +0300

    Initial commit

--- HomeWorks/HW22.py
+++ HomeWorks/HW22.py
@@ -8,3 +8,45 @@
 # метода move, после появления надписи «move» должна появиться надпись
 # «max speed is <max_speed>». Вместо <max_speed> должно выводится значение обязательного атрибума max_speed.
 # Создать по 2 объекта для каждого из классов truck и car, проверить все их методы и атрибуты.
+from time import sleep
+class Auto():
+    def __init__(self, brand, age, mark, color=None, weight=None):
+        self.brand = brand
+        self.age = age
+        self.mark = mark
+        self.color = color
+        self.weight = weight
+    def move(self):
+        print("move")
+    def stop(self):
+        print("stop")
+    def birthday(self):
+        return self.age + 1
+class Truck(Auto):
+    def __init__(self, brand, age, mark, max_load, color=None, weight=None):
+        super().__init__(brand, age, mark, color, weight)
+        self.max_load = max_load
+    def move(self):
+        print("attention")
+    def load(self):
+        sleep(1)
+        print("load")
+        sleep(1)
+class Car(Auto):
+    def __init__(self, brand, age, mark, max_speed, color=None, weight=None):
+        super().__init__(brand, age, mark, color, weight)
+        self.max_speed = max_speed
+    def move(self):
+        print("Max speed is " + str(self.max_speed))
+
+
+auto1 = Car('BMW', 3, "electro", 250)
+auto2 = Truck('Mercedes', 6,"expert", 20, 'blue', 3000)
+print((auto1.brand), (auto1.age), (auto1.mark), (auto1.max_speed), (auto1.color), (auto1.weight))
+auto1.move()
+auto2.move()
+auto2.load()
+print((auto2.brand), (auto2.age), (auto2.mark), (auto2.max_load), (auto2.color), (auto2.weight))
+print("*"*60)
+print("birthday 1: ", auto1.birthday())
+print("birthday 2: ", auto2.birthday())
